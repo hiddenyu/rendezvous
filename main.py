3649327d36f817101b403b6d7528dd9ade90f9b1
hiddenyu <yujunwu@andrew.cmu.edu>
2023-04-09 00:19:53 -0400

    moveable box

--- main.py
+++ main.py
@@ -5,4 +5,31 @@
 #################################################
 
 from cmu_graphics import *
+from player import Player
 import math, copy
+
+def onAppStart(app):
+    app.width, app.height = 1920, 1080
+    app.player = Player(1000, 500)
+
+def redrawAll(app):
+    drawRect(app.player.x, app.player.y, app.player.width, app.player.height, 
+             fill='red')
+
+def onStep(app):
+    pass
+
+def onKeyPress(app, key):
+    if key == 'space':
+        app.player.y -= 50
+
+def onKeyHold(app, keys):
+    if 'right' in keys:
+        app.player.x += 10
+    if 'left' in keys:
+        app.player.x -= 10
+
+def main():
+    runApp()
+
+main()
---
+++ player.py
@@ -0,0 +1,9 @@
+from cmu_graphics import *
+import math, copy
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 50
+        self.height = 100
