92adc5b838be2255d7906d7c6083d65d0e80b1de
hiddenyu <yujunwu@andrew.cmu.edu>
2023-04-08 23:36:45 -0400

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+#################################################
+# term project: rendezvous
+# name: Yujun Wu
+# andrew id: yujunwu
+#################################################
+
+from cmu_graphics import *
+import math, copy
