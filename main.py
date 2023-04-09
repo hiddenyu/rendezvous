94909124024b33f9bc00c64683c76857aeb830ec
hiddenyu <yujunwu@andrew.cmu.edu>
2023-04-09 01:21:46 -0400

    implemented movement + physics start

--- main.py
+++ main.py
@@ -9,25 +9,37 @@ from player import Player
 import math, copy
 
 def onAppStart(app):
+    app.stepsPerSecond = Player.delta
     app.width, app.height = 1920, 1080
     app.player = Player(1000, 500)
 
+    app.isStopped = True
+
 def redrawAll(app):
-    drawRect(app.player.x, app.player.y, app.player.width, app.player.height, 
-             fill='red')
+    drawImage(app.player.sprite, app.player.x, app.player.y)
 
 def onStep(app):
-    pass
+    # app.player.applyGravity()
+    app.player.applyAccel()
+    if app.isStopped:
+        app.player.applyFriction()
 
 def onKeyPress(app, key):
     if key == 'space':
-        app.player.y -= 50
+        app.player.jump()
 
+# friction works only for right movement, left vel since vel alr neg, it = 0
 def onKeyHold(app, keys):
-    if 'right' in keys:
-        app.player.x += 10
-    if 'left' in keys:
-        app.player.x -= 10
+    if 'd' in keys:
+        app.player.moveRight()
+        app.isStopped = False
+    if 'a' in keys:
+        app.player.moveLeft()
+        app.isStopped = False
+
+def onKeyRelease(app, key):
+    if key == 'd' or key == 'a':
+        app.isStopped = True
 
 def main():
     runApp()
--- player.py
+++ player.py
@@ -1,9 +1,38 @@
 from cmu_graphics import *
-import math, copy
+import test
 
 class Player:
+    jumpForce = 500
+    acceleration = 25
+    gravityForce = 10
+    frictionForce = 5
+    delta = 30
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.width = 50
-        self.height = 100
+        self.xVel = 0
+        self.yVel = 0
+        self.sprite = 'test.jpg'
+        self.width, self.height = getImageSize(self.sprite)
+    
+    def applyGravity(self):
+        self.yVel += Player.gravityForce
+        self.y += self.yVel / Player.delta
+    
+    def applyAccel(self):
+        self.x += self.xVel / Player.delta
+    
+    def applyFriction(self):
+        self.xVel -= Player.frictionForce
+        if self.xVel <= 0:
+            self.xVel = 0
+
+    def moveRight(self):
+        self.xVel += Player.acceleration
+    
+    def moveLeft(self):
+        self.xVel -= Player.acceleration
+    
+    def jump(self):
+        self.yVel -= Player.jumpForce
