c42107945a298a045c3fffd52771886a0c0ba7e9
hiddenyu <yujunwu@andrew.cmu.edu>
2023-04-11 14:58:32 -0400

    fixed jump; started collision function

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 #################################################
 
 from cmu_graphics import *
+from PIL import Image
 from player import Player
 import math, copy
 
@@ -12,6 +13,15 @@ def onAppStart(app):
     app.stepsPerSecond = Player.delta
     app.width, app.height = 1920, 1080
     app.player = Player(1000, 0)
+    app.tileMap = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                   [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],]
 
 def redrawAll(app):
     drawImage(app.player.sprite, app.player.x, app.player.y)
@@ -30,6 +40,17 @@ def onKeyHold(app, keys):
         app.player.moveRight()
     if 'a' in keys:
         app.player.moveLeft()
+
+def checkCollision(player, tileMap, tileW, tileH):
+    rows, cols = len(tileMap), len(tileMap[0])
+    for row in range(rows):
+        for col in range(cols):
+            if tileMap[row][col] != 0:
+                left, right = 0 + 120*col, 120 + 120*col
+                top, bot = 0 + 120*row, 120 + 120*row
+                playerLeft, playerTop = app.player.x, app.player.y
+                playerRight = playerLeft + app.player.width
+                playerBot = playerTop + app.player.height
 
 def main():
     runApp()
--- player.py
+++ player.py
@@ -1,13 +1,13 @@
 from cmu_graphics import *
-import test
+from PIL import Image
 
 class Player:
     jumpForce = 500
     acceleration = 50
-    gravityForce = 15
+    gravityForce = 25
     frictionForce = 25
     maxSpeed = 250
-    maxFall = 250
+    maxFall = 500
     delta = 30
 
     def __init__(self, x, y):
@@ -15,9 +15,20 @@ class Player:
         self.y = y
         self.xVel = 0
         self.yVel = 0
-        self.sprite = 'test.jpg'
+        self.sprite = CMUImage(Image.open('test.jpg'))
         self.width, self.height = getImageSize(self.sprite)
     
+    # to animate with sprite strip
+    # app.sprites = []
+    # for i in range(frames):
+    #       frame = Image(sprite.crop((x1, y1, x2, y2)))
+    #       app.sprites.append(frame)
+    # app.spriteCounter = 0
+    # onStep -> app.spriteCounter = (1 + app.spriteCounter) % len(app.sprites) 
+    # could use 2d list, each row being idle, run, etc.
+    #
+    # for gifs, use gif.seek(frame) by looping for frame in range(gif.n_frames)
+
     def applyGravity(self):
         self.yVel += Player.gravityForce
         self.yVel = min(self.yVel, Player.maxFall)
@@ -47,4 +58,4 @@ class Player:
         self.xVel -= Player.acceleration
     
     def jump(self):
-        self.yVel -= Player.jumpForce
+        self.yVel = -Player.jumpForce
